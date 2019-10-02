295310eb9304b2d5c70023ccc8eccd6eb867e508
Danielschuchardt <55810618+Danielschuchardt@users.noreply.github.com>
2019-10-02 16:35:06 -0500

    Youwinmenu and button

--- New 2D Unity Project/Assets/MyStuff/Scripts/Collectible.cs
+++ New 2D Unity Project/Assets/MyStuff/Scripts/Collectible.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets._2D;
 
 public class Collectible : MonoBehaviour
 {
+    private bool _hasBeencollected = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        HandleCollected();  
+        if (!_hasBeencollected && collision.gameObject.GetComponent<PlatformerCharacter2D>())
+        {
+            HandleCollected();
+        }
+    
+
+        if (!_hasBeencollected)
+        {
+            HandleCollected();
+        }
     }
 
     void HandleCollected()
     {
-        CollectibleManager.Instance.HandleCoinCollected(); 
+        _hasBeencollected = true;
         Destroy(gameObject);
+        CollectibleManager.Instance.HandleCoinCollected();
     }
 }
---
+++ New 2D Unity Project/Assets/MyStuff/Scripts/Youwinmenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Youwinmenu : MonoBehaviour
+{
+    public static Youwinmenu Instance;
+    public GameObject container;
+    public Button nextlevelbutton;
+
+    private void OnEnable()
+    {
+        nextlevelbutton.onClick.AddListener(Handlenextlevelpressed);
+    }
+
+    private void OnDisable()
+    {
+        nextlevelbutton.onClick.RemoveListener(Handlenextlevelpressed);
+    }
+
+    private void Handlenextlevelpressed()
+    {
+        int nextsceneindex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextsceneindex);
+    }
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Show()
+    {
+        container.SetActive(true);
+    }
+}
