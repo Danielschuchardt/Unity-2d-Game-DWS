1ca0e1f5347f945d2cd0495b22583423c5d8a2f8
Danielschuchardt <55810618+Danielschuchardt@users.noreply.github.com>
2019-10-09 16:55:17 -0500

    sound and teeth

---
+++ New 2D Unity Project/Assets/MyStuff/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    public AudioSource audioSource;
+
+    public AudioClip coinCollectSound;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject)
+        }
+    }
+    public void PlayCoinCollect()
+    {
+        audioSource.clip = coinCollectSound;
+            audioSource.Play();
+    }
+}
